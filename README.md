a0039bb950761a5620d391c04c8e19a5d601aab9
Wagner Leonardi <leonardiwagner@gmail.com>
2021-05-30 06:48:47 +0200

    improvements

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # podnapisi-subtitle-downloader
 Download subtitles from podnapisi.net
+
+### How to run
+- Have Node.js
+- Start: `npm start`
--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const puppeteer = require('puppeteer');
 
-const BASE_URL = 'https://www.podnapisi.net/en/subtitles/search/?sort=stats.downloads&fps=!23.976'
-const year = '2005-2005'
+// Change the scrap configurations here
+const YEAR_START = 1960
+const YEAR_END = 2021
+const LANGUAGE = 'nl'
 
+
+const BASE_URL = 'https://www.podnapisi.net/en/subtitles/search/?sort=stats.downloads&fps=!23.976'
 const getUrl = (year, page) =>
-  `${BASE_URL}&year=${year}&movie_type=movie&language=!en&order=desc&page=${page}`
+  `${BASE_URL}&year=${year}&movie_type=movie&language=!${LANGUAGE}&order=desc&page=${page}`
 
 const pause = (ms) =>
   new Promise((resolve) => { setTimeout(() => { resolve() }, ms)})
 
-const doIt = async () => {
+const download = async (browser, href) => {
+  const page = await browser.newPage();
+  try{
+    await page.goto(href);
+  } catch {}
+  setTimeout(() => {
+    page.close()
+  }, 3000)
+}
+
+const start = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  for(let year = 2003; year <= 2021; year++){
+  for(let year = YEAR_START; year <= YEAR_END; year++){
     for(let pagination = 1; pagination <= 15; pagination++) {
       const url = getUrl(`${year}-${year}`, pagination)
 
@@ -22,10 +36,10 @@ const doIt = async () => {
       await page.goto(url);
       await page.waitForSelector('footer');
 
-      const downloadSelectors = await page.$$('a[alt="Download subtitles."')
+      const hrefs = await page.$$eval('a[alt="Download subtitles."', as => as.map(a => a.href));
 
-      for(let i = 0; i < downloadSelectors.length; i++) {
-        downloadSelectors[i].click()
+      for(let i = 0; i < hrefs.length; i++) {
+        download(browser, hrefs[i])
         await pause(1000)
       }
 
@@ -37,8 +51,5 @@ const doIt = async () => {
 }
 
 (() => {
-  doIt() 
+  start() 
 })();
-
-
-console.log("wtf")
