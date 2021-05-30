7e295e73cbc87b9b6fefb6a36d4f5b0d07159b43
Wagner Leonardi <leonardiwagner@gmail.com>
2021-05-30 13:30:48 +0200

    fix start script

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 # install node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~./bashrc
+source ~/.bashrc
 nvm install 12
 
 # run app
+mkdir files
 npm i
 npm start
